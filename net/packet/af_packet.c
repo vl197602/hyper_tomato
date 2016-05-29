688826a201c28afdbc3adfb931af68cd3489aaee
psndna88 <psndna88@gmail.com>
2016-05-29 19:38:51 +0530

    net/packet/af_packet: add missing definations of PACKET_FANOUT_RND for function fanout_demux_rnd

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -1250,6 +1250,9 @@ static int packet_rcv_fanout(struct sk_buff *skb, struct net_device *dev,
 	case PACKET_FANOUT_CPU:
 		idx = fanout_demux_cpu(f, skb, num);
 		break;
+	case PACKET_FANOUT_RND:
+        idx = fanout_demux_rnd(f, skb, num);
+        break;
 	case PACKET_FANOUT_ROLLOVER:
 		idx = fanout_demux_rollover(f, skb, 0, (unsigned int) -1, num);
 		break;
@@ -1318,6 +1321,7 @@ static int fanout_add(struct sock *sk, u16 id, u16 type_flags)
 			return -EINVAL;
 	case PACKET_FANOUT_HASH:
 	case PACKET_FANOUT_LB:
+	case PACKET_FANOUT_RND:
 	case PACKET_FANOUT_CPU:
 		break;
 	default:
