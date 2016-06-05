f5f2e954c36ef6ee41f5d0d4e171aa9731294893
Shoaib0597 <Shoaib0595@gmail.com>
2016-06-05 19:20:27 +0530

    Set default values of ThunderCharge: 1250mAh for AC and 1000mAh for USB

--- drivers/power/thundercharge_control.c
+++ drivers/power/thundercharge_control.c
@@ -20,8 +20,8 @@
 #include "thundercharge_control.h"
 
 #define ENABLED             0
-#define AC_CURRENT          1100
-#define USB_CURRENT         500
+#define AC_CURRENT          1250
+#define USB_CURRENT         1000
 #define MAX_VBUS_CURRENT    1500
 #define THUNDERCHARGE       "thundercharge"
 
